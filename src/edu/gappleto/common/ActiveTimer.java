eb80d6f339c41b4fe8e5d91de4b752d67ea152ff
ThePenultimateOne <gappleto97@gmail.com>
2014-11-07 15:37:51 -0500

    Fixed Javadoc formatting

--- src/edu/gappleto/common/ActiveTimer.java
+++ src/edu/gappleto/common/ActiveTimer.java
@@ -14,13 +14,15 @@ import edu.wpi.first.wpilibj.templates.commands.CommandBase;
  * This custom timer will implement the Timer.delay(), provided by WPI, and at
  * the same time update the DriveTrain controls, while still freezing other
  * subsystems.  This will prevent accidental collisions.
- * @author gappleto97
+ * @author ThePenultimateOne
+ * @since Bruce (2.3.0)
  */
 public class ActiveTimer 
 {
     /**
      * This timer should keep the DriveTrain updated, while still delaying other functions.
      * If DriveTrain delay is needed, use WPI's supplied Timer.delay().
+     * @author ThePenultimateOne
      * @param seconds	Double—the amount of time to delay in seconds
      * @since Bruce (2.3.0)
      */
