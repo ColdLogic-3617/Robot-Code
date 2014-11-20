abc1fc674ef427177cbad73777eda2d76cd9f76d
ThePenultimateOne <gappleto97@gmail.com>
2014-11-19 19:38:39 -0500

    Added subsystems

---
+++ src/edu/cold/logic/subsystems/Compressor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cold.logic.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author gappleto97
+ */
+public class Compressor extends Subsystem
+{
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand()
+    {
+        // Set the default command for a subsystem here.
+	//setDefaultCommand(new MySpecialCommand());
+    }
+}
