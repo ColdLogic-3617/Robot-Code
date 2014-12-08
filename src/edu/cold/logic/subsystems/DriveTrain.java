108fe170cddce11ca16e732ef98043409f5ca6d5
ThePenultimateOne <gappleto97@gmail.com>
2014-12-08 15:48:44 -0500

    Added portions of DriveTrain

--- src/edu/cold/logic/subsystems/DriveTrain.java
+++ src/edu/cold/logic/subsystems/DriveTrain.java
@@ -6,6 +6,10 @@
 package edu.cold.logic.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.cold.logic.Var;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.gappleto.common.Logger;
 
 /**
  *
@@ -15,10 +19,48 @@ public class DriveTrain extends Subsystem
 {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+    private Jaguar  frontLeftMotor = new Jaguar(Var.leftMotor1),
+             frontRightMotor = new Jaguar(Var.rightMotor1),
+             rearLeftMotor = new Jaguar(Var.leftMotor2),
+             rearRightMotor = new Jaguar(Var.rightMotor2);
+    private RobotDrive drive;
+    Logger log = new Logger();
+    
+    public DriveTrain() {
+        drive = new RobotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+        log.println("DriveTrain online");
+    }
 
     public void initDefaultCommand()
     {
         // Set the default command for a subsystem here.
 	//setDefaultCommand(new MySpecialCommand());
     }
+    
+     /**
+     * Moves the system forward at specified percent of voltage
+     * @author thayeryates
+     * @param voltage	Double—Percentage of desired voltage
+    */
+    public void GoForward (double voltage)  {
+        drive.setLeftRightMotorOutputs(voltage, voltage);
+        log.println("Set left & right motor to " + voltage);
+    }
+    
+    /**
+     * Moves the system backward at specified percent of voltage
+     * @author thayeryates
+     * @param voltage	Double—Percentage of desired voltage
+    */
+    public void GoBackward (double voltage)  {
+        drive.setLeftRightMotorOutputs(-voltage, -voltage);
+        log.println("Set left & right motor to " + -voltage);
+    }
+    
+     /**
+     * Halts the drivetrain
+     * @author thayeryates
+     * @since Scarab (1.1.1)
+     */
+    
 }
