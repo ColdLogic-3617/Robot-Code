781bd7bd583a93f8fb568285231cea63f517796d
ThayerYates <thayeryates@gmail.com>
2014-12-10 20:32:34 -0500

    Created Loader
    Made the code for the loader.

--- src/edu/cold/logic/subsystems/Loader.java
+++ src/edu/cold/logic/subsystems/Loader.java
@@ -5,14 +5,19 @@
  */
 package edu.cold.logic.subsystems;
 
+import edu.cold.logic.Var;
 import edu.wpi.first.wpilibj.command.Subsystem;
-
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.cold.logic.OI;
 /**
  *
  * @author gappleto97
  */
 public class Loader extends Subsystem
 {
+    Jaguar loader = new Jaguar(Var.loader);
+    OI oi = new OI();
+    private boolean topMove = false;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
@@ -20,5 +25,39 @@ public class Loader extends Subsystem
     {
         // Set the default command for a subsystem here.
 	//setDefaultCommand(new MySpecialCommand());
+        
+    }
+    
+    public void setSpeed(int speed)
+    {
+	topMove = true;
+	if (Math.abs(speed) > 1)
+	    speed = speed / Math.abs(speed);
+	loader.set(speed);
+	Var.log.println("Loader set to " + speed * 100 + "%.");
+    }
+     
+    public void Load()
+    {
+	topMove = true;
+	loader.set(-1);
+	Var.log.println("Loader set to -100%.");
+    }
+        
+    public void Dump()
+    {
+	topMove = true;
+	loader.set(1);
+	Var.log.println("Loader set to 100%.");
+    }
+     
+    public void Stop()
+    {
+	loader.set(0);
+	if (topMove == true)
+	{
+	    topMove = false;
+	    Var.log.println("Loader stopped.");	    
+	}
     }
 }
