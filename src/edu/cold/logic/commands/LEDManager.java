342263418dfc50d66fbbf84f1addad3ebd1c94e2
ThePenultimateOne <gappleto97@gmail.com>
2014-12-12 16:09:04 -0500

    Update LEDManager.java

--- src/edu/cold/logic/commands/LEDManager.java
+++ src/edu/cold/logic/commands/LEDManager.java
@@ -5,19 +5,24 @@
  */
 package edu.cold.logic.commands;
 
+import edu.cold.logic.Var;
+
 /**
  *
- * @author Gabe
+ * @author ThePenultimateOne
  */
 public class LEDManager extends CommandBase {
     
     public LEDManager() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+        requires(LED);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	LED.setEnabled();
+    	LED.setTeam();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -31,10 +36,14 @@ public class LEDManager extends CommandBase {
 
     // Called once after isFinished returns true
     protected void end() {
+        LED.setDisabled();
+        Var.log.println("LED control ended");
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        LED.setDisabled();
+        Var.log.println("LED interrupted");
     }
 }
