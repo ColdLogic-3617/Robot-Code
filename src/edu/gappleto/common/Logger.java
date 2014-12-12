55541848d6fa0fac8bde559707be125834467a3e
ThePenultimateOne <gappleto97@gmail.com>
2014-12-12 14:35:19 -0500

    Added beta static print line

--- src/edu/gappleto/common/Logger.java
+++ src/edu/gappleto/common/Logger.java
@@ -15,7 +15,9 @@ import edu.wpi.first.wpilibj.DriverStation;
  */
 public class Logger
 {
-    private DriverStation Station = DriverStation.getInstance();
+    private static DriverStation Station = DriverStation.getInstance();
+    private static DriverStation Stat = DriverStation.getInstance();
+    
     /**
      * This custom logging tool will add the timestamp* and battery voltage to
      * any logging message for the robot.
@@ -28,6 +30,26 @@ public class Logger
      * @since Bruce (2.2.2)
      */
     public void println(String message)
+    {
+	double v = Stat.getBatteryVoltage();
+	double t = Stat.getMatchTime();
+	v = (double)(((int)(v * 10000))/10000.0);
+	t = (double)(((int)(t * 10000))/10000.0);
+	System.out.println("[" + t + "] v=" + v + ": " + message);
+    }
+    
+    /**
+     * This custom logging tool will add the timestamp* and battery voltage to
+     * any logging message for the robot.
+     * *Note: on teleop, the time will start at 15 seconds.  This is not
+     * necessarily the actual time.
+     * @author gappleto97
+     * @param message	String—the message to be logged
+     * @see PrintStream
+     * @see DriverStation
+     * @since Bruce (2.2.2)
+     */
+    public static void printlnBeta(String message)
     {
 	double v = Station.getBatteryVoltage();
 	double t = Station.getMatchTime();
