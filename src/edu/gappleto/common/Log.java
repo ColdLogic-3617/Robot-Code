4802c9767132cf2de5bda6fe261ad1d8cf46f5e0
ThePenultimateOne <gappleto97@gmail.com>
2014-12-25 13:54:57 -0500

    Added javadocs to beta methods

--- src/edu/gappleto/common/Log.java
+++ src/edu/gappleto/common/Log.java
@@ -72,13 +72,15 @@ public class Log	{
     
     /**
      * This custom logging tool will add the timestamp* and battery voltage to
-     * any logging message for the robot.
+     * any logging message for the robot. It should also mirror this log entry
+     * on the "LCD" portion of the Drive Station.
      * *Note: on teleop, the time will start at 15 seconds.  This is not
      * necessarily the actual time.
      * @author Gabe
      * @param message	String—the message to be logged
      * @see PrintStream
      * @see DriverStation
+     * @see DriverStationLCD
      * @since Bruce (2.4.2)
      */
     public static void printlnConsBeta(String message)	{
@@ -91,6 +93,15 @@ public class Log	{
 	putToLCD(message);
     }
     
+    /**
+     * Internal method to put data onto the LCD. This should not be publicly
+     * accessible, as it is only meant to be accessed from the traditional
+     * log method. This may change in the future.
+     * @author Gabe
+     * @param message	String-the message to be logged
+     * @see DriverStationLCD
+     * @since Bruce (2.6.0)
+     */
     private static void putToLCD(String message)	{
     	if (currentLine % 6 == 1)	{
 	    Station.println(Line1, 1, message);
