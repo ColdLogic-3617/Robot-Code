0653c8f138934df1ff6cbefd1c09e704fba75923
ThePenultimateOne <gappleto97@gmail.com>
2014-12-25 13:50:24 -0500

    Added beta of LCD mirroring

--- src/edu/gappleto/common/Log.java
+++ src/edu/gappleto/common/Log.java
@@ -7,15 +7,27 @@
 package edu.gappleto.common;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStationLCD;
+import edu.wpi.first.wpilibj.DriverStationLCD.Line;
 
 /**
  * This is a custom logging tool, which will include timestamp and voltage.
  * @author Gabe
  * @since Bruce (2.2.2)
  */
-public class Log
-{
+public class Log	{
+	
     private static final DriverStation Station = DriverStation.getInstance();
+    private static final DriverStationLCD LCD = DriverStationLCD.getInstance();
+    
+    private int currentLine = 1;
+    
+    private static final Line    line1 = DriverStationLCD.Line.kUser1,
+				 line2 = DriverStationLCD.Line.kUser2,
+				 line3 = DriverStationLCD.Line.kUser3,
+				 line4 = DriverStationLCD.Line.kUser4,
+				 line5 = DriverStationLCD.Line.kUser5,
+				 line6 = DriverStationLCD.Line.kUser6;
     
     /**
      * This custom logging tool will add the timestamp* and battery voltage to
@@ -29,8 +41,7 @@ public class Log
      * @since Bruce (2.2.2)
      * @deprecated
      */
-    public void printlnDep(String message)
-    {
+    public void printlnDep(String message)	{
 	double v = Station.getBatteryVoltage();
 	double t = Station.getMatchTime();
 	v = (double)(((int)(v * 10000))/10000.0);
@@ -49,12 +60,62 @@ public class Log
      * @see DriverStation
      * @since Bruce (2.4.2)
      */
-    public static void println(String message)
-    {
+    public static void println(String message)	{
 	double v = Station.getBatteryVoltage();
 	double t = Station.getMatchTime();
 	v = (double)(((int)(v * 10000))/10000.0);
 	t = (double)(((int)(t * 10000))/10000.0);
 	System.out.println("[" + t + "] v=" + v + ": " + message);
+    }
+    
+    
+    
+    /**
+     * This custom logging tool will add the timestamp* and battery voltage to
+     * any logging message for the robot.
+     * *Note: on teleop, the time will start at 15 seconds.  This is not
+     * necessarily the actual time.
+     * @author Gabe
+     * @param message	String—the message to be logged
+     * @see PrintStream
+     * @see DriverStation
+     * @since Bruce (2.4.2)
+     */
+    public static void printlnConsBeta(String message)	{
+	double v = Station.getBatteryVoltage();
+	double t = Station.getMatchTime();
+	v = (double)(((int)(v * 10000))/10000.0);
+	t = (double)(((int)(t * 10000))/10000.0);
+	message = "[" + t + "] v=" + v + ": " + message;
+	System.out.println(message);
+	putToLCD(message);
+    }
+    
+    private static void putToLCD(String message)	{
+    	if (currentLine % 6 == 1)	{
+	    Station.println(Line1, 1, message);
+	}
+	else if (currentLine % 6 == 2)	{
+	    Station.println(Line2, 1, message);
+	}
+	else if (currentLine % 6 == 3)	{
+	    Station.println(Line3, 1, message);
+	}
+	else if (currentLine % 6 == 4)	{
+	    Station.println(Line4, 1, message);
+	}
+	else if (currentLine % 6 == 5)	{
+	    Station.println(Line5, 1, message);
+	}
+	else	{
+	    Station.println(Line6, 1, message);
+	}
+	currentLine = currentLine + 1;
+	if (currentLine > 6)	{
+	    currentLine = currentLine % 6;
+	}
+	if (message.length() > 22)
+		putToLCD(message.substring(22));
+	LCD.updateLCD();
     }
 }
